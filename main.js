c3e944a710561091bf03d41fdf20398a1b82ba3d
Jamie Bort <jamiebort@gmail.com>
2018-01-15 14:14:58 -0500

    backing up all work until now.

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // <script>
 // Get the modal
-var modal = document.getElementById('myModal');
+let modal1 = document.getElementById('myModal1');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn1 = document.getElementById("myBtn1");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span1 = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+btn1.onclick = function() {
+    modal1.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modal1.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -29,13 +29,13 @@ window.onclick = function(event) {
 
 // ============ Take two
 // Get the modal
-var modal2 = document.getElementById('myModal2');
+let modal2 = document.getElementById('myModal2');
 
 // Get the button that opens the modal
-var btn2 = document.getElementById("myBtn2");
+let btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("close2")[0];
+let span2 = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 btn2.onclick = function() {
@@ -50,6 +50,6 @@ span2.onclick = function() {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal2.style.display = "none";
     }
 }
