1f37b91d6c329218fbad5fa35d96b4f9cd7c8322
Jamie Bort <jamiebort@gmail.com>
2018-01-19 12:02:13 -0500

    Edited files with a dummy button to work on the style of the showcase buttons.

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // From: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal2
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal1 = document.getElementById('myModal1');
+var modalTwo = document.getElementById('myModalTwo');
 var modal2 = document.getElementById('myModal2');
 var modal3 = document.getElementById('myModal3');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn1 = document.getElementById("myBtn1");
+var btnTwo = document.getElementById("myBtnTwo");
 var btn2 = document.getElementById("myBtn2");
 var btn3 = document.getElementById("myBtn3");
 
@@ -15,8 +17,12 @@ var btn3 = document.getElementById("myBtn3");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+btnTwo.onclick = function() {
+  modalTwo.style.display = "block";
 }
 
 btn2.onclick = function() {
@@ -37,8 +43,12 @@ btn3.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+
+  if (event.target == modalTwo) {
+    modalTwo.style.display = "none";
   }
 
   if (event.target == modal2) {
