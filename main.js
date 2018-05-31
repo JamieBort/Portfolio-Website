b712b4a5f75d3c6a3658036bab97e701669f2763
Jamie Bort <jamiebort@gmail.com>
2018-05-30 21:05:50 -0400

    Finished coding.

--- main.js
+++ main.js
@@ -5,10 +5,12 @@
 // Get the modal
 var modal = document.getElementById('myModal');
 var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -22,6 +24,10 @@ btn2.onclick = function() {
   modal2.style.display = "block";
 }
 
+btn3.onclick = function() { 
+  modal3.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
@@ -29,6 +35,10 @@ span.onclick = function() {
 
 span.onclick = function() {
   modal2.style.display = "none";
+}
+
+span.onclick = function() {
+  modal3.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -40,6 +50,10 @@ window.onclick = function(event) {
     if (event.target == modal2) {
       modal2.style.display = "none";
   }
+
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+}
 }
 
 // For the copyright
