dce08efd17a2cb5ab50760bf84cb0532cc64b291
Jamie Bort <jamiebort@gmail.com>
2018-11-13 15:19:37 -0500

    Modified CSS file using flexbox.

---
+++ main.js
@@ -0,0 +1,7 @@
+// For the copyright
+
+var copyright = document.getElementById('copyright');
+var d = new Date()
+// document.write(d.getFullYear())
+var a = d.getFullYear()
+copyright.innerHTML=`&copy` + a
