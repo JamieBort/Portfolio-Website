5453f9f3f8f120b1913a8a0337fa9d54ca955126
Jamie Bort <jamiebort@gmail.com>
2020-10-14 12:09:13 -0400

    Removed the old/ directory.

--- old/20181126/main.js
+++
@@ -1,7 +0,0 @@
-// For the copyright
-
-var copyright = document.getElementById('copyright');
-var d = new Date()
-// document.write(d.getFullYear())
-var a = d.getFullYear()
-copyright.innerHTML=`&copy` + a
