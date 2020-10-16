c18fa678ccd909334158a405e3a5a90af153f197
Jamie Bort <jamiebort@gmail.com>
2020-10-16 10:47:36 -0400

    The header and footer are fixed and are being used as Web Components.

--- source.js
+++ source.js
@@ -2,18 +2,21 @@
 class Header extends HTMLElement{
     connectedCallback() {
         this.innerHTML=`
-                            <header>
-                                    <div id="left">
-                                        <div>Jamie Bort</div>
-                                    </div>
-                                    <div id="right">
-                                        <button><a href="./index.html">Bio</a></button>
-                                        <button><a href="./blog.html">Blog</a></button>
-                                        <button><a href="./index.html">Projects</a></button>
-                                        <button><a href="./index.html">Fun Stuff</a></button>
-                                        <button><a href="./index.html">Contact Form</a></button>
-                                    </div>
-                                </header>
+        <div class="header">
+            <div id="header-cont">
+                <div id="left">
+                    <p>Name</p>
+                </div>
+                <div id="right">
+                    <button>Bio</button>
+                    <button>Blog</button>
+                    <button>Projects</button>
+                    <button>Resources</button>
+                    <button>Fun Stuff</button>
+                    <button>Contact Form</button>
+                </div>
+            </div>
+        </div>
                             `;
                         }
                     }
@@ -22,7 +25,9 @@ class Header extends HTMLElement{
 class Footer extends HTMLElement{
     connectedCallback() {
         this.innerHTML=`
-                            <footer id="footer"><span>Copyright info</span></footer>
+        <div class="footer">
+        Footer
+    </div>
                         `;
                     }
                 }
