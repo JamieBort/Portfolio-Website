41914b3a7f59f577f367605b76a5d73264a587b4
Jamie Bort <jamiebort@gmail.com>
2020-10-20 14:10:52 -0400

    Created the js/table.js file.

---
+++ js/table.js
@@ -0,0 +1,9 @@
+// For the skills table
+data = {
+    "Q1": [1,2,3,2],
+    "Q2": [5,6,7,6],
+    "Q3": [9,10,11,10]
+};
+console.log(data)
+var tbody = document.getElementById('tbody');
+tbody.innerHTML="Hello";
