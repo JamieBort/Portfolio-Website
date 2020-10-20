fc3c9368cae0b7c16d5513b2866b9a2d9f97cfd5
Jamie Bort <jamiebort@gmail.com>
2020-10-20 14:01:55 -0400

    Created the html/, js/, and sytles/ directories. Moved files into them. And refactored accordingly.

--- js/source.js
+++ js/source.js
@@ -1,12 +1,12 @@
 // for header
-class Header extends HTMLElement{
+class Header extends HTMLElement {
     connectedCallback() {
-        this.innerHTML=`
+        this.innerHTML = `
         <div class="header">
             <div id="header-cont">
                 <div id="left">
                     <button class="head_button">
-                        <a class="head_a" href="./index.html#title">Jamie Bort</a>
+                        <a class="head_a" href="../html/index.html#title">Jamie Bort</a>
                     </button>
                 </div>
                 <div id="right">
@@ -14,27 +14,27 @@ class Header extends HTMLElement{
                         <a class="head_a"  href="./index.html#bio">Bio</a>
                     </button>
                     `
-                    // below is commented out because the Blog is not ready to show to the public.
-                    // <button class="head_button"><a class="head_a"  href="./blog.html#blog">Blog</a></button>
-                    +`
+            // below is commented out because the Blog is not ready to show to the public.
+            // <button class="head_button"><a class="head_a"  href="./blog.html#blog">Blog</a></button>
+            + `
                     
                     <button class="head_button">
                         <a class="head_a"  href="./index.html#projects">Projects</a>
                     </button>
                     `
-                    // below is commented out because the Resources is not ready to show to the public.
-                    // <button class="head_button"><a class="head_a"  href="./resources.html">Resources</a></button>
-                    +`
+            // below is commented out because the Resources is not ready to show to the public.
+            // <button class="head_button"><a class="head_a"  href="./resources.html">Resources</a></button>
+            + `
                     `
-                    // below is commented out because the Fun Stuff is not ready to show to the public.
-                    // <button class="head_button"><a class="head_a"  href="./fun_stuff.html">Fun Stuff</a></button>
-                    +`
+            // below is commented out because the Fun Stuff is not ready to show to the public.
+            // <button class="head_button"><a class="head_a"  href="./fun_stuff.html">Fun Stuff</a></button>
+            + `
                     `
-                    // below is commented out because the Resume page is not ready to show to the public.
-                    // <button class="head_button">
-                        // <a class="head_a"  href="./resume.html">Resume </a>
-                    // </button>
-                    +`
+            // below is commented out because the Resume page is not ready to show to the public.
+            // <button class="head_button">
+            // <a class="head_a"  href="./resume.html">Resume </a>
+            // </button>
+            + `
                     
                     <button class="head_button">
                         <a class="head_a"  href="./index.html#connect_with_me">Connect with me</a>
@@ -43,19 +43,29 @@ class Header extends HTMLElement{
             </div>
         </div>
                             `;
-                        }
-                    }
+    }
+}
+
+// For the skills table
+data = {
+    "Q1": [1,2,3,2],
+    "Q2": [5,6,7,6],
+    "Q3": [9,10,11,10]
+};
+console.log(data)
+var tbody = document.getElementById('tbody')
+tbody.innerHTML="Hello"
 
 // for footer
-class Footer extends HTMLElement{
+class Footer extends HTMLElement {
     connectedCallback() {
-        this.innerHTML=`
+        this.innerHTML = `
         <div class="footer">
             <span id="copyright">This should give the copyright date.</span>
     </div>
                         `;
-                    }
-                }
+    }
+}
 
 // last lines
 customElements.define('header-web-component', Header);
@@ -64,7 +74,7 @@ customElements.define('footer-web-component', Footer);
 
 // For the copyright
 var copyright = document.getElementById('copyright');
-copyright.innerHTML=`&copy` + new Date().getFullYear();
+copyright.innerHTML = `&copy` + new Date().getFullYear();
 // var date = new Date()
 // document.write(d.getFullYear())
 // var fullYearDate = date.getFullYear()
