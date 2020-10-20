fb8e604ab0948ad88367dd6c303fbc6a3057e2ab
Jamie Bort <jamiebort@gmail.com>
2020-10-20 14:10:29 -0400

    Renamed js/source.js to js/web_components.js

--- js/web_components.js
+++ js/web_components.js
@@ -46,16 +46,6 @@ class Header extends HTMLElement {
     }
 }
 
-// For the skills table
-data = {
-    "Q1": [1,2,3,2],
-    "Q2": [5,6,7,6],
-    "Q3": [9,10,11,10]
-};
-console.log(data)
-var tbody = document.getElementById('tbody')
-tbody.innerHTML="Hello"
-
 // for footer
 class Footer extends HTMLElement {
     connectedCallback() {
