f5e7aea244e54e7c532670988bf1369143ef1f8e
Jamie Bort <jamiebort@gmail.com>
2020-10-22 14:51:31 -0400

    Working on the JavaScript logic for the skillset table.

--- js/table.js
+++ js/table.js
@@ -1,5 +1,7 @@
 // For the skills table
 
+
+// array of objects
 const skillSets = [
     {
         "item": "HTML5",
@@ -221,7 +223,188 @@ const skillSets = [
         "strength": "Intermediate",
         "type": "Software Topics",
     },
-]
+];
+// object of objects
+const skillSets2 = {
+    "HTML5": {
+        "strength": "Expert",
+        "type": "Language",
+    },
+    "CSS3": {
+        "strength": "Expert",
+        "type": "Language",
+    },
+    "Python": {
+        "strength": "Intermediate",
+        "type": "Language",
+    },
+    "Bash": {
+        "strength": "Expert",
+        "type": "Language",
+    },
+    "Java": {
+        "strength": "Intermediate",
+        "type": "Language",
+    },
+    "Git":
+    {
+        "strength": "Expert",
+        "type": "Language",
+    },
+    "C Sharp":
+    {
+        "strength": "Learning/Some Experience",
+        "type": "Language",
+    },
+    "PHP": {
+        "strength": "Want to learn",
+        "type": "Language",
+    },
+    "R": {
+        "strength": "Want to learn",
+        "type": "Language",
+    },
+    "React.js": {
+        "strength": "Intermediate",
+        "type": "Frameworks and Libraries",
+    },
+    "React Native": {
+        "strength": "Intermediate",
+        "type": "Frameworks and Libraries",
+    },
+    "Angular": {
+        "strength": "Learning/Some Experience",
+        "type": "Frameworks and Libraries",
+    },
+    "Spring Boot": {
+        "strength": "Intermediate",
+        "type": "Frameworks and Libraries",
+    },
+    ".NET": {
+        "strength": "Learning/Some Experience",
+        "type": "Frameworks and Libraries",
+    },
+    "D3.js": {
+        "strength": "Learning/Some Experience",
+        "type": "Frameworks and Libraries",
+    },
+    "Node.js": {
+        "strength": "Intermediate",
+        "type": "Frameworks and Libraries",
+    },
+    "Mustache-Express MVC": {
+        "strength": "Intermediate",
+        "type": "Frameworks and Libraries",
+    },
+    "SQL": {
+        "strength": "Intermediate",
+        "type": "Databases",
+    },
+    "MongoDB": {
+        "strength": "Learning/Some Experience",
+        "type": "Databases",
+    },
+    "PostreSQL": {
+        "strength": "Learning/Some Experience",
+        "type": "Databases",
+    },
+    "Mongoose": {
+        "strength": "Learning/Some Experience",
+        "type": "Databases",
+    },
+    "Jira": {
+        "strength": "Expert",
+        "type": "Software & Cloud Platforms",
+    },
+    "Confluence": {
+        "strength": "Expert",
+        "type": "Software & Cloud Platforms",
+    },
+    "Maple": {
+        "strength": "Intermediate",
+        "type": "Software & Cloud Platforms",
+    },
+    "Matlab": {
+        "strength": "Intermediate",
+        "type": "Software & Cloud Platforms",
+    },
+    "Linux": {
+        "strength": "Want to learn",
+        "type": "Software & Cloud Platforms",
+    },
+    "AWS": {
+        "strength": "Want to learn",
+        "type": "Software & Cloud Platforms",
+    },
+    "Heroku": {
+        "strength": "Want to learn",
+        "type": "Software & Cloud Platforms",
+    },
+    "Apache Maven": {
+        "strength": "Want to learn",
+        "type": "Software & Cloud Platforms",
+    },
+    "Visual Studio Code": {
+        "strength": "Expert",
+        "type": "Methodologies, Practices, & IDEs",
+    },
+    "Intellij": {
+        "strength": "Expert",
+        "type": "Methodologies, Practices, & IDEs",
+    },
+    "Kanban": {
+        "strength": "Want to learn",
+        "type": "Methodologies, Practices, & IDEs",
+    },
+    "Agile": {
+        "strength": "Intermediate",
+        "type": "Methodologies, Practices, & IDEs",
+    },
+    "Scrum": {
+        "strength": "Intermediate",
+        "type": "Methodologies, Practices, & IDEs",
+    },
+    "Xcode": {
+        "strength": "Intermediate",
+        "type": "Methodologies, Practices, & IDEs",
+    },
+    "Android Studio": {
+        "strength": "Intermediate",
+        "type": "Methodologies, Practices, & IDEs",
+    },
+    "Test Driven Development (TDD)": {
+        "strength": "Intermediate",
+        "type": "Methodologies, Practices, & IDEs",
+    },
+    "AJAX": {
+        "strength": "Learning/Some Experience",
+        "type": "Software Topics",
+    },
+    "GraphQL": {
+        "strength": "Learning/Some Experience",
+        "type": "Software Topics",
+    },
+    "Soap APIs": {
+        "strength": "Learning/Some Experience",
+        "type": "Software Topics",
+    },
+    "RESTful Routes": {
+        "strength": "Intermediate",
+        "type": "Software Topics",
+    },
+    "Event Listeners": {
+        "strength": "Intermediate",
+        "type": "Software Topics",
+    },
+    "DOM manipulation": {
+        "strength": "Intermediate",
+        "type": "Software Topics",
+    },
+    "Template Literals": {
+        "strength": "Intermediate",
+        "type": "Software Topics",
+    },
+};
 
 // import {skillSets} from '../skill_sets.json';
 // import skillSets from '../skill_sets.json';
@@ -240,13 +423,13 @@ const skillSets = [
 // console.log(skillSets);
 // console.log(skillSets[0]);
 // console.log(skillSets[2].item);
-var myArray=[];
-for (let index = 0; index < skillSets.length; index++) {
-    const element = skillSets[index].item;
-    myArray.push(skillSets[index].item);
-    console.log(element);
-}
-console.log(myArray);
+// var myArray = [];
+// for (let index = 0; index < skillSets.length; index++) {
+//     const element = skillSets[index].item;
+//     myArray.push(skillSets[index].item);
+//     console.log(element);
+// }
+// console.log(myArray);
 
 // fetch(skillSets)
 //   .then(response => response.json())
@@ -260,21 +443,13 @@ console.log(myArray);
 
 // ******************************************************
 //             The below is for table.html
+// from: https://stackoverflow.com/q/50996553/8210460
 // ******************************************************
 
-// data = {
-//     "Q1": [1,2,3,2],
-//     "Q2": [5,6,7,6],
-//     "Q3": [9,10,11,10]
-// };
-// console.log(data);
-// var tbody = document.getElementById('tbody');
-// tbody.innerHTML=`<p>Hello</p>`;
-
 data = {
-    "Q1": [1,2,3,2],
-    "Q2": [5,6,7,6],
-    "Q3": [9,10,11,10],
+    "Q1": [1, 2, 3, 2],
+    "Q2": [5, 6, 7, 6],
+    "Q3": [9, 10, 11, 10],
     'Q4': [], // test case 1: no data
     'Q5': [1], // test case 2: only answer field
     'Q6': ['A', 'B', 'C'],// test case 3: for string
@@ -283,40 +458,65 @@ data = {
 // For Q4, Q5, you can added some extra codes to uses default values intead, or ignore this row.
 
 // formmat the data first, add some validators if neccessary
-function formatAdapter (data) {
-  return Object.entries(data).map((item) => {
-  let newItem = {}
-  newItem[item[0]] = {'options': item[1].slice(0, item[1].length -1),
-    'answer': item[-1]} // assuming the last element is the answer, [0:last] is the options
-  return [item[0], item[1].slice(0, item[1].length -1), item[1].slice(-1)[0]]
-})
+function formatAdapter(data) {
+    return Object.entries(data).map((item) => {
+        let newItem = {}
+        newItem[item[0]] = {
+            'options': item[1].slice(0, item[1].length - 1),
+            'answer': item[-1]
+        } // assuming the last element is the answer, [0:last] is the options
+        return [item[0], item[1].slice(0, item[1].length - 1), item[1].slice(-1)[0]]
+    })
 }
 let formmatedData = formatAdapter(data)
 
 var tbody = document.getElementById('tbody');
 
 // generate the header
-tbody.innerHTML += "<tr>" + 
-                   "<th id='q'>" + "Question" + "</th>" +
-                   "<th id='o'>" + "Options" + "</th>" +
-                   "<th id='ca'>" + "Correct Answer" + "</th>" +
-                   "</tr>"
-// generate the rows(Html) for each questions
+tbody.innerHTML += "<tr>" +
+    "<th id='q'>" + "Question" + "</th>" +
+    "<th id='o'>" + "Options" + "</th>" +
+    "<th id='ca'>" + "Correct Answer" + "</th>" +
+    "</tr>"
 
+// generate the rows(Html) for each questions
 let rowHtmls = formmatedData.map((item) => {
-  let row = '<tr><td rowspan="'+(item[1].length || 1) +'">'+item[0]+'</td>'
-            + '<td>'+item[1][0]+'</td>'
-            + '<td rowspan="'+(item[1].length || 1)+'">'+item[2]+'</td></tr>'
-  row += item[1].slice(1).reduce((pre, cur) => {
-    return pre + '<tr><td>'+cur+'</td></tr>'
-  }, '')
-  return row
+    let row = '<tr><td rowspan="' + (item[1].length || 1) + '">' + item[0] + '</td>'
+        + '<td>' + item[1][0] + '</td>'
+        + '<td rowspan="' + (item[1].length || 1) + '">' + item[2] + '</td></tr>'
+    row += item[1].slice(1).reduce((pre, cur) => {
+        return pre + '<tr><td>' + cur + '</td></tr>'
+    }, '')
+    return row
 })
 
 // combine header(html) and rows(html)
 tbody.innerHTML += rowHtmls.reduce((pre, cur) => {
-  return pre+cur
+    return pre + cur
 }, '')
 
+// ******************************************************
+//             The above is for table.html
+// from: https://stackoverflow.com/q/50996553/8210460
+// ******************************************************
+
+// ******************************************************
+//             My take on the above.
+// ******************************************************
+
+
+tbody2.innerHTML += `<tr>
+                        <th>blank</th>
+                        <th id='q'>Language</th>
+                        <th id='o'>Frameworks & Libraries</th>
+                        <th id='ca'>Databases</th>
+                        <th>Software & Cloud Platforms</th>
+                        <th id='q'>Methodologies, Practices, & IDEs</th>
+                    </tr>`;
+
+console.log(skillSets2);
+console.log(skillSets2[".NET"]);
+console.log(skillSets2.HTML5.type);
+if (skillSets2.HTML5.strength=="Expert"){console.log("Expert!!")};
 
 // export table.js;
