07cdaf5e4d8e5fd2c32d7731d29a5a07a63a5910
Jamie Bort <jamiebort@gmail.com>
2020-10-30 18:31:20 -0400

    Added this composer.json file and this index.php file so that I could host this portfolio on Heroku. See the comment in the index.php file.

---
+++ index.php
@@ -0,0 +1,3 @@
+<!-- Added this file and composer.json using this https://medium.com/@winnieliang/how-to-run-a-simple-html-css-javascript-application-on-heroku-4e664c541b0b tutorial so that I could host this portfolio on heroku. -->
+
+<?php include_once("./html/index.html"); ?>
