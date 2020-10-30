41a0737aaa688d81a0dad909af3aa27d211c7770
Jamie Bort <jamiebort@gmail.com>
2020-10-30 18:43:52 -0400

    Modified the index.php file. And copied home.html to index.html.

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <!-- Added this file and composer.json using this https://medium.com/@winnieliang/how-to-run-a-simple-html-css-javascript-application-on-heroku-4e664c541b0b tutorial so that I could host this portfolio on heroku. -->
 
-<?php include_once("home.html"); ?>
+<?php include_once("index.html"); ?>
