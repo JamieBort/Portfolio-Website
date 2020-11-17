60ef92cf20a721e6d75eede75438821f45b5b073
Jamie Bort <jamiebort@gmail.com>
2020-11-17 14:45:00 -0500

    Tweeking the Connect With Me spans.

--- js/connect_with_me.js
+++ js/connect_with_me.js
@@ -1,9 +1,10 @@
 var lets = document.getElementById('lets');
 // var myArray=["Let's connect.", "Let's collaborate.", "Let's pair program.", "Let's Build Something Together"];
+lets.innerHTML="Pair Program."; // Initial phrase while we're wating for the function to trigger.
 var myArray=["Connect.", "Collaborate.", "Pair Program.", "Build Something Together."];
 var t=0;
 window.setInterval(function(){
     t++;
     lets.innerHTML=myArray[t%myArray.length];
     // console.log(myArray[t%myArray.length]);
-}, 2000);
+}, 4000);
