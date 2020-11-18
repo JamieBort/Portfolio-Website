14b7328d26f0003db381f32c87b332737e25b9c1
Jamie Bort <jamiebort@gmail.com>
2020-11-18 08:32:42 -0500

    Added Active highlight to navigation button.

--- js/web_components.js
+++ js/web_components.js
@@ -15,16 +15,39 @@ class Head extends HTMLElement {
 class Header extends HTMLElement {
     // constructor() {
     //     super();
+        
+
+
     //     var right = document.getElementById("right");
     //     var a = right.getElementsByClassName("head_a");
     //     for (var i = 0; i < a.length; i++) {
-    //         a[i].addEventListener("click", function() {
-    //       var current = document.getElementsByClassName("active");
-    //       current[0].className = current[0].className.replace(" active", "");
-    //       this.className += " active";
-    //       });
-    //     }
+    //         a[i].addEventListener("click", function () {
+    //         var current = document.getElementsByClassName("active");
+    //         current[0].className = current[0].className.replace(" active", "");
+    //         this.className += " active";
+    //     });
     // }
+    
+    // var JamieBort = document.getElementById("JamieBort");
+    // JamieBort.addEventListener("click", function () {
+    //     var bio_link = document.getElementById("bio_link");
+    //     var current = document.getElementsByClassName("active");
+    //     current[0].className = current[0].className.replace(" active", "");
+    //     bio_link.className += " active";
+    // });
+
+
+    // }
+    
+
+    // constructor() {
+    //     super();
+    //     const shadow = this.attachShadow({mode: 'closed'});
+    //     const s = document.createElement('script');
+    //     // s.textContent = 'alert("hello");';
+    //     s.textContent = 'console.log("hello");';
+    //     shadow.appendChild(s);
+    //   }
 
     connectedCallback() {
         this.innerHTML = `
@@ -38,14 +61,14 @@ class Header extends HTMLElement {
             + `
                     </div>
                 <div id="right">
-                    <a id="bio" class="head_a active"  href="./index.html#bio">Bio</a>
+                    <a id="bio_link" class="head_a active"  href="./index.html#bio">Bio</a>
     `            // below is commented out because the Blog is not ready to show to the public.
             // <a class="head_a"  href="./blog.html#blog">Blog</a></button>
             + `
                     <a class="head_a"  href="./index.html#projects">Projects</a>
                         `+
             // below is commented out because the Resources is not ready to show to the public.
-            // <a class="head_a"  href="./resources.html">Resources</a>
+            // <a id="resources" class="head_a"  href="./resources.html">Resources</a>
 
             // below is commented out because the Fun Stuff is not ready to show to the public.
             // <a class="head_a"  href="./fun_stuff.html">Fun Stuff</a>
@@ -57,7 +80,7 @@ class Header extends HTMLElement {
             // <a class="head_a"  href="./talks.html">Resume </a>
             `
                    <a class="head_a"  href="./index.html#connect_with_me">Connect with me</a>
-                   <a id="resources" class="head_a"  href="./resources.html">Resources</a>
+                   
                 </div>
             </nav>
         </header>
@@ -90,11 +113,56 @@ class BlogEntry extends HTMLElement {
     }
 }
 
+// script
+
+class scripts extends HTMLElement {
+constructor() {
+    super();
+    const shadow = this.attachShadow({mode: 'open'});
+    const s = document.createElement('script');
+
+    // s.textContent = 'alert("hello");';
+    // s.textContent = 'console.log("hello");';
+    
+    var right = document.getElementById("right");
+    var a = right.getElementsByClassName("head_a");
+    for (var i = 0; i < a.length; i++) {
+        a[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
+
+    var JamieBort = document.getElementById("JamieBort");
+    JamieBort.addEventListener("click", function () {
+        var bio_link = document.getElementById("bio_link");
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        bio_link.className += " active";
+    });
+    
+    // var resources = document.getElementById("resources");
+    //     resources.addEventListener("click", function () {
+    //         var resources = document.getElementById("resources");
+    //         var current = document.getElementsByClassName("active");
+    //         // var current = document.getElementById("bio_link");
+    //         current[0].className = current[0].className.replace(" active", "");
+    //         // resources.className += " active";
+    //         this.className += " active";
+    //     });
+
+shadow.appendChild(s);
+
+  }
+}
+
 // last lines
 customElements.define('head-web-component', Head);
 customElements.define('header-web-component', Header);
 customElements.define('footer-web-component', Footer);
 customElements.define('blog-entry-web-component', BlogEntry);
+customElements.define('scripts-web-component', scripts);
 
 
 // For the copyright
