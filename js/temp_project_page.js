08a96e8758da1025cb5374fd9530518ac60697ce
Jamie Bort <jamiebort@gmail.com>
2021-06-19 11:34:40 -0400

    Create the html/temp_project_page.html and js/temp_project_page.js files.

---
+++ js/temp_project_page.js
@@ -0,0 +1,155 @@
+// created for the api calls in the project_card_api branch
+
+// the following egoist_array was creating using this api call:
+// https://gh-pinned-repos-5l2i19um3.vercel.app/?username=jamiebort
+// using this repo:
+// https://github.com/egoist/gh-pinned-repos
+// more info on it found here:
+// https://trello.com/c/RyfM5Z0E/1124-have-the-cards-themselves-created-populated-based-on-the-pinned-items-in-github?menu=filter&filter=label:none
+
+egoist_array = [{ "owner": "jamiebort", "repo": "LearningDirectory", "link": "https://github.com/jamiebort/LearningDirectory", "description": "The purpose of this directory is to house all the code I write as I learn new technologies and hone my skills. This is to help keep my GitHub account organized.", "language": "JavaScript", "stars": 0, "forks": 0 }, { "owner": "jamiebort", "repo": "PersonalDashboard", "link": "https://github.com/jamiebort/PersonalDashboard", "description": "The repository for a static website that will house charts, graphs, and the like of things I need to keep track of.", "language": "JavaScript", "stars": 0, "forks": 0 }, { "owner": "jamiebort", "repo": "jamiebort.github.io", "link": "https://github.com/jamiebort/jamiebort.github.io", "description": "My Portfolio Website", "language": "JavaScript", "stars": 0, "forks": 0 }, { "owner": "jamiebort", "repo": "iPhysicist-CSSLayoutFlexbox", "link": "https://github.com/jamiebort/iPhysicist-CSSLayoutFlexbox", "description": "Finish the styling for the 'i Physicist' blog mockup. Skills using CSS, HTML, GitHub, Bash.", "language": "HTML", "stars": 0, "forks": 0 }, { "owner": "jamiebort", "repo": "PuppyLove", "link": "https://github.com/jamiebort/PuppyLove", "description": "This project tests your basic knowledge of CSS and styling.", "language": "HTML", "stars": 0, "forks": 0 }, { "owner": "jamiebort", "repo": "CreateAUserdirectory", "link": "https://github.com/jamiebort/CreateAUserdirectory", "description": "Using Express and Mustache-express to implement MVC.", "language": "CSS", "stars": 0, "forks": 0 }]
+
+// https://api.github.com/repos/jamiebort/CodeSnippetOrganizer
+
+CodeSnippetOrganizer_obj={
+    "id": 102218042,
+    "node_id": "MDEwOlJlcG9zaXRvcnkxMDIyMTgwNDI=",
+    "name": "CodeSnippetOrganizer",
+    "full_name": "JamieBort/CodeSnippetOrganizer",
+    "private": false,
+    "owner": {
+      "login": "JamieBort",
+      "id": 4405730,
+      "node_id": "MDQ6VXNlcjQ0MDU3MzA=",
+      "avatar_url": "https://avatars.githubusercontent.com/u/4405730?v=4",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/JamieBort",
+      "html_url": "https://github.com/JamieBort",
+      "followers_url": "https://api.github.com/users/JamieBort/followers",
+      "following_url": "https://api.github.com/users/JamieBort/following{/other_user}",
+      "gists_url": "https://api.github.com/users/JamieBort/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/JamieBort/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/JamieBort/subscriptions",
+      "organizations_url": "https://api.github.com/users/JamieBort/orgs",
+      "repos_url": "https://api.github.com/users/JamieBort/repos",
+      "events_url": "https://api.github.com/users/JamieBort/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/JamieBort/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    "html_url": "https://github.com/JamieBort/CodeSnippetOrganizer",
+    "description": "Build an organizer for holding your and others' code snippets.",
+    "fork": false,
+    "url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer",
+    "forks_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/forks",
+    "keys_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/keys{/key_id}",
+    "collaborators_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/collaborators{/collaborator}",
+    "teams_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/teams",
+    "hooks_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/hooks",
+    "issue_events_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/issues/events{/number}",
+    "events_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/events",
+    "assignees_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/assignees{/user}",
+    "branches_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/branches{/branch}",
+    "tags_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/tags",
+    "blobs_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/git/blobs{/sha}",
+    "git_tags_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/git/tags{/sha}",
+    "git_refs_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/git/refs{/sha}",
+    "trees_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/git/trees{/sha}",
+    "statuses_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/statuses/{sha}",
+    "languages_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/languages",
+    "stargazers_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/stargazers",
+    "contributors_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/contributors",
+    "subscribers_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/subscribers",
+    "subscription_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/subscription",
+    "commits_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/commits{/sha}",
+    "git_commits_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/git/commits{/sha}",
+    "comments_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/comments{/number}",
+    "issue_comment_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/issues/comments{/number}",
+    "contents_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/contents/{+path}",
+    "compare_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/compare/{base}...{head}",
+    "merges_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/merges",
+    "archive_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/{archive_format}{/ref}",
+    "downloads_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/downloads",
+    "issues_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/issues{/number}",
+    "pulls_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/pulls{/number}",
+    "milestones_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/milestones{/number}",
+    "notifications_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/notifications{?since,all,participating}",
+    "labels_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/labels{/name}",
+    "releases_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/releases{/id}",
+    "deployments_url": "https://api.github.com/repos/JamieBort/CodeSnippetOrganizer/deployments",
+    "created_at": "2017-09-02T19:11:32Z",
+    "updated_at": "2021-06-07T15:55:26Z",
+    "pushed_at": "2021-06-07T15:55:23Z",
+    "git_url": "git://github.com/JamieBort/CodeSnippetOrganizer.git",
+    "ssh_url": "git@github.com:JamieBort/CodeSnippetOrganizer.git",
+    "clone_url": "https://github.com/JamieBort/CodeSnippetOrganizer.git",
+    "svn_url": "https://github.com/JamieBort/CodeSnippetOrganizer",
+    "homepage": null,
+    "size": 38,
+    "stargazers_count": 0,
+    "watchers_count": 0,
+    "language": "JavaScript",
+    "has_issues": true,
+    "has_projects": true,
+    "has_downloads": true,
+    "has_wiki": true,
+    "has_pages": false,
+    "forks_count": 0,
+    "mirror_url": null,
+    "archived": false,
+    "disabled": false,
+    "open_issues_count": 1,
+    "license": null,
+    "forks": 0,
+    "open_issues": 1,
+    "watchers": 0,
+    "default_branch": "master",
+    "temp_clone_token": null,
+    "network_count": 0,
+    "subscribers_count": 0
+  }
+
+//   this works. now to iterate through the object and then come back to this.
+  const ul = document.getElementById("egoist_array");
+  fetch('https://api.github.com/repos/jamiebort/CodeSnippetOrganizer')
+  .then(response => response.json())
+//   .then(response => ul.innerHTML=JSON.stringify(response, null, 4))
+  .then(data => ul.innerHTML=JSON.stringify(data, null, 4))
+//   .then(data => ul.innerHTML=data)
+  .then(data => console.log(data));
+
+// const object = { a: 1, b: 2, c: 3 };
+// for (const property in object) {
+//   console.log(`${property}: ${object[property]}`);
+// }
+
+//   const ul = document.getElementById("egoist_array");
+//   fetch("https://api.github.com/repos/jamiebort/CodeSnippetOrganizer")
+//   .then(function (response) {
+//       console.log("Inside response function");
+//     //   ul.innerHTML=JSON.stringify(CodeSnippetOrganizer_obj, null, 4);
+//       ul.innerHTML=JSON.stringify(response, null, 4);
+//     console.log(JSON.stringify(response, null, 4));
+//     return response.json();
+//   })
+// //   .then(function (data) {
+// //     appendData(data);
+// //   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
+
+
+
+
+  // //   https://stackoverflow.com/a/8294549/8210460
+// // JSON.stringify({ foo: "sample", bar: "sample" }, null, 4)
+// console.log(JSON.stringify({ foo: "sample", bar: "sample" }, null, 4));
+// // const ul = document.getElementsByTagName(ul)[0];
+// const ul = document.getElementById("egoist_array");
+// // ul.innerHTML=egoist_array[0]["owner"];
+// // ul.innerHTML=JSON.stringify({ foo: "sample", bar: "sample" }, null, 4);
+// ul.innerHTML=JSON.stringify(CodeSnippetOrganizer_obj, null, 4);
+
+// const ul2 = document.getElementById("egoist_array02");
+// ul2.innerHTML=JSON.stringify(CodeSnippetOrganizer_obj, null, 40);
