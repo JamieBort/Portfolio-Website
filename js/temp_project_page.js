bc9e3448d99f90e4e838667423b4b6c12afd923d
Jamie Bort <jamiebort@gmail.com>
2021-06-19 16:25:24 -0400

    Work on the temp_project_page.js file. Getting closer.

--- js/temp_project_page.js
+++ js/temp_project_page.js
@@ -11,31 +11,31 @@ egoist_array = [{ "owner": "jamiebort", "repo": "LearningDirectory", "link": "ht
 
 // https://api.github.com/repos/jamiebort/CodeSnippetOrganizer
 
-CodeSnippetOrganizer_obj={
+CodeSnippetOrganizer_obj = {
     "id": 102218042,
     "node_id": "MDEwOlJlcG9zaXRvcnkxMDIyMTgwNDI=",
     "name": "CodeSnippetOrganizer",
     "full_name": "JamieBort/CodeSnippetOrganizer",
     "private": false,
     "owner": {
-      "login": "JamieBort",
-      "id": 4405730,
-      "node_id": "MDQ6VXNlcjQ0MDU3MzA=",
-      "avatar_url": "https://avatars.githubusercontent.com/u/4405730?v=4",
-      "gravatar_id": "",
-      "url": "https://api.github.com/users/JamieBort",
-      "html_url": "https://github.com/JamieBort",
-      "followers_url": "https://api.github.com/users/JamieBort/followers",
-      "following_url": "https://api.github.com/users/JamieBort/following{/other_user}",
-      "gists_url": "https://api.github.com/users/JamieBort/gists{/gist_id}",
-      "starred_url": "https://api.github.com/users/JamieBort/starred{/owner}{/repo}",
-      "subscriptions_url": "https://api.github.com/users/JamieBort/subscriptions",
-      "organizations_url": "https://api.github.com/users/JamieBort/orgs",
-      "repos_url": "https://api.github.com/users/JamieBort/repos",
-      "events_url": "https://api.github.com/users/JamieBort/events{/privacy}",
-      "received_events_url": "https://api.github.com/users/JamieBort/received_events",
-      "type": "User",
-      "site_admin": false
+        "login": "JamieBort",
+        "id": 4405730,
+        "node_id": "MDQ6VXNlcjQ0MDU3MzA=",
+        "avatar_url": "https://avatars.githubusercontent.com/u/4405730?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/JamieBort",
+        "html_url": "https://github.com/JamieBort",
+        "followers_url": "https://api.github.com/users/JamieBort/followers",
+        "following_url": "https://api.github.com/users/JamieBort/following{/other_user}",
+        "gists_url": "https://api.github.com/users/JamieBort/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/JamieBort/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/JamieBort/subscriptions",
+        "organizations_url": "https://api.github.com/users/JamieBort/orgs",
+        "repos_url": "https://api.github.com/users/JamieBort/repos",
+        "events_url": "https://api.github.com/users/JamieBort/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/JamieBort/received_events",
+        "type": "User",
+        "site_admin": false
     },
     "html_url": "https://github.com/JamieBort/CodeSnippetOrganizer",
     "description": "Build an organizer for holding your and others' code snippets.",
@@ -107,49 +107,185 @@ CodeSnippetOrganizer_obj={
     "temp_clone_token": null,
     "network_count": 0,
     "subscribers_count": 0
-  }
+}
+
+//   https://api.github.com/repos/jamiebort/CreateAUserdirectory
+CreateAUserdirectory_obj = {
+    "id": 99573781,
+    "node_id": "MDEwOlJlcG9zaXRvcnk5OTU3Mzc4MQ==",
+    "name": "CreateAUserdirectory",
+    "full_name": "JamieBort/CreateAUserdirectory",
+    "private": false,
+    "owner": {
+        "login": "JamieBort",
+        "id": 4405730,
+        "node_id": "MDQ6VXNlcjQ0MDU3MzA=",
+        "avatar_url": "https://avatars.githubusercontent.com/u/4405730?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/JamieBort",
+        "html_url": "https://github.com/JamieBort",
+        "followers_url": "https://api.github.com/users/JamieBort/followers",
+        "following_url": "https://api.github.com/users/JamieBort/following{/other_user}",
+        "gists_url": "https://api.github.com/users/JamieBort/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/JamieBort/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/JamieBort/subscriptions",
+        "organizations_url": "https://api.github.com/users/JamieBort/orgs",
+        "repos_url": "https://api.github.com/users/JamieBort/repos",
+        "events_url": "https://api.github.com/users/JamieBort/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/JamieBort/received_events",
+        "type": "User",
+        "site_admin": false
+    },
+    "html_url": "https://github.com/JamieBort/CreateAUserdirectory",
+    "description": "Using Express and Mustache-express to implement MVC.",
+    "fork": false,
+    "url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory",
+    "forks_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/forks",
+    "keys_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/keys{/key_id}",
+    "collaborators_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/collaborators{/collaborator}",
+    "teams_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/teams",
+    "hooks_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/hooks",
+    "issue_events_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/issues/events{/number}",
+    "events_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/events",
+    "assignees_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/assignees{/user}",
+    "branches_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/branches{/branch}",
+    "tags_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/tags",
+    "blobs_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/git/blobs{/sha}",
+    "git_tags_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/git/tags{/sha}",
+    "git_refs_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/git/refs{/sha}",
+    "trees_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/git/trees{/sha}",
+    "statuses_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/statuses/{sha}",
+    "languages_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/languages",
+    "stargazers_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/stargazers",
+    "contributors_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/contributors",
+    "subscribers_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/subscribers",
+    "subscription_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/subscription",
+    "commits_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/commits{/sha}",
+    "git_commits_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/git/commits{/sha}",
+    "comments_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/comments{/number}",
+    "issue_comment_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/issues/comments{/number}",
+    "contents_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/contents/{+path}",
+    "compare_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/compare/{base}...{head}",
+    "merges_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/merges",
+    "archive_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/{archive_format}{/ref}",
+    "downloads_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/downloads",
+    "issues_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/issues{/number}",
+    "pulls_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/pulls{/number}",
+    "milestones_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/milestones{/number}",
+    "notifications_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/notifications{?since,all,participating}",
+    "labels_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/labels{/name}",
+    "releases_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/releases{/id}",
+    "deployments_url": "https://api.github.com/repos/JamieBort/CreateAUserdirectory/deployments",
+    "created_at": "2017-08-07T12:05:12Z",
+    "updated_at": "2021-01-19T14:25:44Z",
+    "pushed_at": "2021-01-19T14:25:41Z",
+    "git_url": "git://github.com/JamieBort/CreateAUserdirectory.git",
+    "ssh_url": "git@github.com:JamieBort/CreateAUserdirectory.git",
+    "clone_url": "https://github.com/JamieBort/CreateAUserdirectory.git",
+    "svn_url": "https://github.com/JamieBort/CreateAUserdirectory",
+    "homepage": "",
+    "size": 1854,
+    "stargazers_count": 0,
+    "watchers_count": 0,
+    "language": "CSS",
+    "has_issues": true,
+    "has_projects": true,
+    "has_downloads": true,
+    "has_wiki": true,
+    "has_pages": true,
+    "forks_count": 0,
+    "mirror_url": null,
+    "archived": false,
+    "disabled": false,
+    "open_issues_count": 0,
+    "license": null,
+    "forks": 0,
+    "open_issues": 0,
+    "watchers": 0,
+    "default_branch": "master",
+    "temp_clone_token": null,
+    "network_count": 0,
+    "subscribers_count": 0
+}
 
 //   this works. now to iterate through the object and then come back to this.
-  const ul = document.getElementById("egoist_array");
-  fetch('https://api.github.com/repos/jamiebort/CodeSnippetOrganizer')
-  .then(response => response.json())
-//   .then(response => ul.innerHTML=JSON.stringify(response, null, 4))
-  .then(data => ul.innerHTML=JSON.stringify(data, null, 4))
-//   .then(data => ul.innerHTML=data)
-  .then(data => console.log(data));
+const ul = document.getElementById("egoist_array");
+var li = document.createElement("li");
+fetch('https://api.github.com/repos/jamiebort/CreateAUserdirectory')
+    .then(response => response.json())
+    .then(data => {
 
-// const object = { a: 1, b: 2, c: 3 };
-// for (const property in object) {
-//   console.log(`${property}: ${object[property]}`);
-// }
+        li = document.createElement("li");
+        li.innerHTML = "name: " + data.name;
+        ul.appendChild(li);
 
-//   const ul = document.getElementById("egoist_array");
-//   fetch("https://api.github.com/repos/jamiebort/CodeSnippetOrganizer")
-//   .then(function (response) {
-//       console.log("Inside response function");
-//     //   ul.innerHTML=JSON.stringify(CodeSnippetOrganizer_obj, null, 4);
-//       ul.innerHTML=JSON.stringify(response, null, 4);
-//     console.log(JSON.stringify(response, null, 4));
-//     return response.json();
-//   })
-// //   .then(function (data) {
-// //     appendData(data);
-// //   })
-//   .catch(function (err) {
-//     console.log(err);
-//   });
+        li = document.createElement("li");
+        li.innerHTML = "description: " + data.description;
+        ul.appendChild(li);
+
+        // ul2 = document.createElement('ul');
+        // ul.appendChild(ul2);
+
+        li = document.createElement("li");
+        // li.innerHTML="language- css: "+data.language;
+        ul.appendChild(li);
+        console.log("data.language: ", data.language);
+        console.log("data.languages_url: ", data.languages_url);
+    });
+
+ul2 = document.createElement('ul');
+ul.appendChild(ul2);
+
+fetch('https://api.github.com/repos/JamieBort/CreateAUserdirectory/languages')
+    .then(response => response.json())
+    .then(data => {
+        for (const property in data) {
+            li = document.createElement("li");
+            li.innerHTML = property + ":&emsp;" + data[property];
+            ul2.appendChild(li);
+        }
+        console.log("data: ", data);
+    });
+
+console.log("****************************************")
+// console.log("egoist_array: ", egoist_array)
+// // console.log("egoist_array[0]['repo']: ", egoist_array[0]["repo"])
+for (const x of egoist_array) {
+    console.log(x["repo"]);
 
 
 
 
-  // //   https://stackoverflow.com/a/8294549/8210460
-// // JSON.stringify({ foo: "sample", bar: "sample" }, null, 4)
-// console.log(JSON.stringify({ foo: "sample", bar: "sample" }, null, 4));
-// // const ul = document.getElementsByTagName(ul)[0];
-// const ul = document.getElementById("egoist_array");
-// // ul.innerHTML=egoist_array[0]["owner"];
-// // ul.innerHTML=JSON.stringify({ foo: "sample", bar: "sample" }, null, 4);
-// ul.innerHTML=JSON.stringify(CodeSnippetOrganizer_obj, null, 4);
+    fetch('https://api.github.com/repos/jamiebort/' + x["repo"])
+        .then(response => response.json())
+        .then(data => {
+            console.log("data.name: ", data.name);
+            console.log("data.description: ", data.description);
 
-// const ul2 = document.getElementById("egoist_array02");
-// ul2.innerHTML=JSON.stringify(CodeSnippetOrganizer_obj, null, 40);
+            // for (const property in data) {
+            //     li = document.createElement("li");
+            //     li.innerHTML = property + ":&emsp;" + data[property];
+            //     ul2.appendChild(li);
+            // }
+            // console.log("data: ", data);
+        });
+
+
+        fetch('https://api.github.com/repos/JamieBort/'+x["repo"]+'/languages')
+        .then(response => response.json())
+        .then(data => {
+
+            // for (const property in data) {
+            //     li = document.createElement("li");
+            //     li.innerHTML = property + ":&emsp;" + data[property];
+            //     ul2.appendChild(li);
+            // }
+
+            console.log("data: ", data);
+            // console.log("data[0]: ", data[Object]);
+            for (const property in data) {
+                console.log("property: ",property)
+            }
+        });
+
+}
