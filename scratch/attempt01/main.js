11d5e211152ab0b0d8f4ad534b2b335b168a5690
Jamie Bort <jamiebort@gmail.com>
2023-04-05 11:55:02 -0600

    Push to view progress online.

--- scratch/attempt01/main.js
+++ scratch/attempt01/main.js
@@ -9,18 +9,11 @@ const leftHeaderButton = () => (window.location = "#top");
 
 const rightHeaderButton = () => toggleNavMenu();
 
-const jumpToSection1 = () => {
-  window.location = "#section01";
+const jumpToSection = (param) => {
+  window.location = "#" + param;
   toggleNavMenu();
 };
-const jumpToSection2 = () => {
-  window.location = "#section02";
-  toggleNavMenu();
-};
-const jumpToSection3 = () => {
-  window.location = "#section03";
-  toggleNavMenu();
-};
+
 const toggleNavMenu = () => {
   const menu = document.getElementById("menu");
   menu.style.display === "none"
@@ -31,11 +24,9 @@ const toggleNavMenu = () => {
 const closeNavMenu = () =>
   (document.getElementById("menu").style.display = "none");
 
-// // Add event listener to page.
-// const el = document.getElementById("content");
-// el.addEventListener("click", closeNavMenu, false); // why false? --> "A boolean value indicating that the listener should be invoked at most once after being added. If true, the listener would be automatically removed when invoked. If not specified, defaults to false."
-
 // Add event listener to page.
+// why false? --> "A boolean value indicating that the listener should be invoked at most once after being added.
+// If true, the listener would be automatically removed when invoked. If not specified, defaults to false."
 document
   .getElementById("content")
-  .addEventListener("click", closeNavMenu, false); // why false? --> "A boolean value indicating that the listener should be invoked at most once after being added. If true, the listener would be automatically removed when invoked. If not specified, defaults to false."
+  .addEventListener("click", closeNavMenu, false);
