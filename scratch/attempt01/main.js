93b2ba60754b9bfa7963b6610c13d04b029c1e37
Jamie Bort <jamiebort@gmail.com>
2023-04-05 11:09:13 -0600

    Push to view progress online.

---
+++ scratch/attempt01/main.js
@@ -0,0 +1,18 @@
+// ./main.js
+
+const leftHeaderButton = () => {
+  window.location = "#top";
+};
+const rightHeaderButton = () => {
+  const menu = document.getElementById("menu");
+  menu.style.display = "block";
+};
+const jumpToSection1 = () => {
+  window.location = "#section01";
+};
+const jumpToSection2 = () => {
+  window.location = "#section02";
+};
+const jumpToSection3 = () => {
+  window.location = "#section03";
+};
