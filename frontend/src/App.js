b0a13e33b657064041ec803ea8a9b81363afc686
Jamie Bort <jamiebort@gmail.com>
2024-01-31 17:36:31 -0500

    Make minor text change to home page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,11 +11,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>I am amazing</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>I am amazing</h1>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
