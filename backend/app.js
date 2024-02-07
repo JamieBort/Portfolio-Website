466f38dc3b3ae2d781685309b02406e3169210c9
Jamie Bort <jamiebort@gmail.com>
2024-02-07 14:36:44 -0500

    Prepare to deploy backend via Render.com.

--- backend/app.js
+++ backend/app.js
@@ -61,7 +61,13 @@ const query = `
 
 // NOTE: Local files do not work. They have to be served.
 // NOTE: http://127.0.0.1:5500 is sufficient for ./jamiebort.github.io/frontend/index.html in VS Code.
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5500", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:3000",
+  "http://127.0.0.1:5500",
+  "http://localhost:3000",
+  "https://jamiebort-github-io-frontend-react.onrender.com",
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
