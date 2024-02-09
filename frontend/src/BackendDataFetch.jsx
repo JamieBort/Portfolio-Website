349ddf36a6d813a72a53f84f96e8819e4d3590d1
Jamie Bort <jamiebort@gmail.com>
2024-02-09 10:31:07 -0500

    Add console.logs to observe behavior.

--- frontend/src/BackendDataFetch.jsx
+++ frontend/src/BackendDataFetch.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function BackendDataFetch() {
   // URL of local backend.
-  // const URL = `http://localhost:3000/`;
+  // const URL = `http://localhost:3001/`;
   // URL of hosted backend.
   const URL = `https://backend-jamiebort-github-io.onrender.com/`;
   const [repos, setRepos] = useState();
@@ -10,36 +10,35 @@ export default function BackendDataFetch() {
   const [loading, setLoading] = useState(true);
 
   // The api call.
+  // TODO: Clean up these catches and errors.
   const apiCall = () => {
-    console.log("apiCall:", Date());
+    // console.log("apiCall:", Date()); // Let's see that apiCall() was called.
     fetch(URL)
       .then((response) => {
-        console.log("response:", response);
+        // console.log("response:", response); // Let's see the response in the console.
         if (response.ok) {
           // Yay! We have data back from the back end.
           return response.json();
         }
         // We have a problem.
-        throw new Error("Something went wrong.");
+        throw new Error("apiCall thrown error.");
       })
       .then((responseJson) => {
-        // console.log("responseJson:", responseJson); // Let's see the response in the console.
-        // Updating the "repos" state.
+        // console.log("responseJson:", responseJson); // Let's see the response body in the console.
+        // Updating the "repos" state with setRepos().
         setRepos(responseJson);
         // Only after the "repos" data is updated will we change the "loading" status to false.
         setLoading(false);
       })
-      .catch((error) => {
-        // Uh oh
-        console.log("api call error:", error);
-      });
+      // Uh oh
+      .catch((error) => console.log("apiCall catch error:", error));
   };
 
   // Attempt to fetch data from the back end.
   try {
     // TODO: Add dependency to the useEffect.
     useEffect(() => {
-      console.log("INITIAL FETCH via useEffect in front end");
+      // console.log("INITIAL FETCH via useEffect in front end"); // useEffect() is called.
       apiCall();
     }, []);
   } catch (error) {
@@ -48,6 +47,7 @@ export default function BackendDataFetch() {
   }
 
   // Attempt to keep the backend from going to sleep.
+  // This setInterval() is called every x minutes/(1000*60).
   setInterval(() => {
     console.log("attempting to fetch via setInterval in front end");
     apiCall();
@@ -57,6 +57,7 @@ export default function BackendDataFetch() {
     // Mapping through the data from the api call.
     var repo = repos.map((item) => {
       // Destructuring the item.node object.
+
       const { description, forkCount, id, homepageUrl, name, stargazerCount, url } = item.node;
       // Returning each repo detail.
       return (
