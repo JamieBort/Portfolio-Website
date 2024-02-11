ab3bb82413aa3569772335ecbdeda8346168eea0
Jamie Bort <jamiebort@gmail.com>
2024-02-10 19:30:12 -0500

    Attempt to work with CoreUI.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Bio from "./components/Bio";
 import Project from "./components/Project";
 import ConnectWithMe from "./components/ConnectWithMe";
 // import NextUIcard from "./components/cards/NextUIcard";
+// import CoreUIcard from "./components/cards/CoreUIcard";
 
 export default function App() {
   return (
     <>
       <h1>Jamie Bort Software Engineer</h1>
       {/* <NextUIcard /> */}
+      {/* <CoreUIcard /> */}
       <Bio title="Bio" />
       <Project title="Projects" />
       <ConnectWithMe title="Connect With Me" />
--- frontend/src/components/cards/CoreUIcard.jsx
+++ frontend/src/components/cards/CoreUIcard.jsx
@@ -1,1 +1,17 @@
-// from: https://coreui.io/react/docs/components/card/
+// import PropTypes from "prop-types";
+// import Section from "./Section";
+import CCard from "@coreui/react";
+
+export default function CoreUIcard() {
+  //   console.log(props.title);
+  return (
+    <>
+      <h3>dumb</h3>
+      <CCard style={{ width: "18rem" }}></CCard>
+    </>
+  );
+}
+
+// Bio.propTypes = {
+//   title: PropTypes.string,
+// };
