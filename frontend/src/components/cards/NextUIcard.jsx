0a3baf561ff3671810acde273a89b2d578167335
Jamie Bort <jamiebort@gmail.com>
2024-02-14 09:31:34 -0500

    Modify the frontend/src/components/cards/CoreUIcard.jsx and frontend/src/components/cards/NextUIcard.jsx files in preparation for deleting them.

--- frontend/src/components/cards/NextUIcard.jsx
+++ frontend/src/components/cards/NextUIcard.jsx
@@ -1,50 +1,52 @@
-// import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
+// // // TODO: remove this file from the project. Delete it.
 
-// NOTE: need to follow these installation instructions first: https://nextui.org/docs/frameworks/vite
+// // import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 
-// NextUI exports 4 card-related components:
-// Card: The main component to display a card.
-// CardHeader: Commonly used for the title of a card.
-// CardBody: The content of the card.
-// CardFooter: Commonly used for actions.
+// // NOTE: need to follow these installation instructions first: https://nextui.org/docs/frameworks/vite
 
-// import React from "react";
-// import { Card, CardBody } from "@nextui-org/react";
+// // NextUI exports 4 card-related components:
+// // Card: The main component to display a card.
+// // CardHeader: Commonly used for the title of a card.
+// // CardBody: The content of the card.
+// // CardFooter: Commonly used for actions.
 
+// // import React from "react";
+// // import { Card, CardBody } from "@nextui-org/react";
+
+// // export default function NextUIcard() {
+// //   return (
+// //     <Card>
+// //       <CardBody>
+// //         <p>Make beautiful websites regardless of your design experience.</p>
+// //       </CardBody>
+// //     </Card>
+// //   );
+// // }
+
+// // import React from "react";
+// import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
+
+// // export default function App() {
 // export default function NextUIcard() {
 //   return (
-//     <Card>
+//     <Card className="max-w-[400px]">
+//       <CardHeader className="flex gap-3">
+//         <Image alt="nextui logo" height={40} radius="sm" src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4" width={40} />
+//         <div className="flex flex-col">
+//           <p className="text-md">NextUI</p>
+//           <p className="text-small text-default-500">nextui.org</p>
+//         </div>
+//       </CardHeader>
+//       <Divider />
 //       <CardBody>
 //         <p>Make beautiful websites regardless of your design experience.</p>
 //       </CardBody>
+//       <Divider />
+//       <CardFooter>
+//         <Link isExternal showAnchorIcon href="https://github.com/nextui-org/nextui">
+//           Visit source code on GitHub.
+//         </Link>
+//       </CardFooter>
 //     </Card>
 //   );
 // }
-
-// import React from "react";
-import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
-
-// export default function App() {
-export default function NextUIcard() {
-  return (
-    <Card className="max-w-[400px]">
-      <CardHeader className="flex gap-3">
-        <Image alt="nextui logo" height={40} radius="sm" src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4" width={40} />
-        <div className="flex flex-col">
-          <p className="text-md">NextUI</p>
-          <p className="text-small text-default-500">nextui.org</p>
-        </div>
-      </CardHeader>
-      <Divider />
-      <CardBody>
-        <p>Make beautiful websites regardless of your design experience.</p>
-      </CardBody>
-      <Divider />
-      <CardFooter>
-        <Link isExternal showAnchorIcon href="https://github.com/nextui-org/nextui">
-          Visit source code on GitHub.
-        </Link>
-      </CardFooter>
-    </Card>
-  );
-}
