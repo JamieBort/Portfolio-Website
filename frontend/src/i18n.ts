7b78060bf44ba4d90ba9fef55368a5ec9d14c498
Jamie Bort <jamiebort@gmail.com>
2025-07-10 14:46:01 -0400

    Add comments.

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,12 +1,9 @@
 // The config file.
 // TODO: move this file into a ./utils/ directory.
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next"; // Internationalization framework for React & React Native, which is based on i18next.
 import Backend from "i18next-http-backend"; // Allows me to serve the translations from the back end. Right now they reside in `./frontend/public/locales/`.
-import LanguageDetector from "i18next-browser-languagedetector";
-// console.log(initReactI18next);
-// console.log(Backend);
-// console.log(LanguageDetector);
+import LanguageDetector from "i18next-browser-languagedetector"; // Used to detect user language in the browser, with support for cookie, sessionStorage, localStorage, navigator, and more.
 
 i18n
   .use(initReactI18next) // Passes i18n down to react-i18next
